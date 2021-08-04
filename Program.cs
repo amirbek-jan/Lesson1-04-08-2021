d8fc50e23887f8d5de6316f634413bef15360788
Amirbek <mahmudiamirbek@gmail.com>
2021-08-04 13:06:18 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Lesson1ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Первое ДЗ
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(a * b);
+
+            Console.WriteLine($"Результат = {Math.Round(result, 2)}");
+            */
+
+            /*
+            Второе ДЗ
+            Console.Write("A = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("B = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("C = ");
+            double c = double.Parse(Console.ReadLine());
+
+            double ac = Math.Abs(a - c);
+            double bc = Math.Abs(b - c);
+
+            double result = Math.Abs(ac + bc);
+
+            Console.WriteLine($"AC = {Math.Round(ac, 2)}");
+            Console.WriteLine($"BC = {Math.Round(bc, 2)}");
+
+            Console.WriteLine($"AC+BC = {Math.Round(result, 2)}");
+            */
+
+            /*
+            Третье ДЗ
+            Console.Write("x1 = ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine($"Результат = {Math.Round(result, 2)}");
+            */
+
+            /*
+            Четвёртое дз
+            Console.Write("Число = ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int a = (n % 10) * 10 + (n / 10);
+
+            Console.WriteLine($"Результат = {a}");
+            */
+
+            /*
+            Пятое дз
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = n / 60;
+
+            Console.WriteLine($"Результат : {result}");
+            */
+
+            /*
+            Шестое ДЗ
+            Console.Write("K = ");
+            int a = int.Parse(Console.ReadLine());
+
+            int result = a % 7;
+
+            Console.WriteLine($"Номер дня недели: {result}");
+            */
+        }
+    }
+}
